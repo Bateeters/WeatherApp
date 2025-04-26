c894fac50ef04a660fc2b0b59101ea484195f9f5
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-04-26 12:17:41 -0400

    separated JS funcs. location, date, temp set up

---
+++ src/render.js
@@ -0,0 +1,45 @@
+import './startingUi.js';
+import { getDateTime } from './date.js';    
+import { locationRender } from './location.js';
+import { getTemp, highLowTemps } from './temperature.js';
+
+export function renderWeather(data) {
+    locationRender(data[0]);
+    getDateTime(data[1].datetimeEpoch, data[3]);
+    getTemp(data[1], data[3]);
+    highLowTemps(data[2]);
+
+    /*
+    const weatherContainer = document.querySelector('#weatherContainer');
+    weatherContainer.innerHTML = 
+        `<h2>${getDateTime (data[1].datetimeEpoch)}</h2>
+        <h2>Location: ${data[0]}</h2>
+        <p>Temperature: ${data[1].feelslike}°C</p>
+        <p>${data[2].feelslikemax} / ${data[2].feelslikemin}`;*/
+
+ 
+    const imageUrl = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/58c79610addf3d4d91471abbb95b05e96fb43019/SVG/1st%20Set%20-%20Monochrome/${data[1].icon}.svg`;
+
+    const image = document.querySelector('#image')
+    const imageStyle = image.style;
+    image.src = imageUrl;
+    imageStyle.display = 'initial';
+
+    console.log(data);
+
+    const mainDiv = document.querySelector('#mainDiv');
+    const conditions = document.querySelector('#conditions');
+    let weatherConditions = data[1].conditions;
+    conditions.classList.remove('hidden');
+    conditions.textContent = weatherConditions;
+
+    if (data[1].icon == 'clear-day') {
+        mainDiv.style.backgroundImage = `linear-gradient(135deg,rgb(238, 223, 147), rgb(230, 200, 126),rgb(172, 202, 231),rgb(124, 197, 228))`;
+    } else if (data[1].icon == 'partly-cloudy-day') {
+        mainDiv.style.backgroundImage = `linear-gradient(135deg,rgb(202, 211, 224), rgb(202, 211, 224),rgb(172, 202, 231),rgb(124, 197, 228))`;
+    } else if (data[1].icon == 'rain') {
+        mainDiv.style.backgroundImage = `linear-gradient(135deg,rgb(75, 101, 152), rgb(117, 150, 216)`;
+    } else if (data[1].icon == 'cloudy') {
+        mainDiv.style.backgroundImage = `linear-gradient(135deg,rgb(172, 194, 203), rgb(195, 220, 235)`;
+    }
+}
